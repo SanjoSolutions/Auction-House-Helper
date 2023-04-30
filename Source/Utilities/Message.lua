d7e3a49d4eeb9c73c0644bc03022a41c7805919c
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-30 13:04:58 +0200

    Spacing

--- Source/Utilities/Message.lua
+++ Source/Utilities/Message.lua
@@ -1,6 +1,6 @@
 function AuctionHouseHelper.Utilities.Message(message)
   print(
-    LIGHTBLUE_FONT_COLOR:WrapTextInColorCode("AuctionHouseHelper: ")
+    LIGHTBLUE_FONT_COLOR:WrapTextInColorCode("Auction House Helper: ")
     .. message
   )
 end
